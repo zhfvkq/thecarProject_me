702dd5ff223656165d6828feffcdb2408dd72b60
DESKTOP-IAPPT26\DYKim <hamoey@naver.com>
2021-06-14 12:55:16 +0900

    06/24프로젝트 소스, 프레젠테이션, 산출물 _ 최종

--- thecarProject/workspace/SpringthecarProject_admin/src/main/java/com/thecar/admin/login/dao/LoginDao.java
+++
@@ -1,9 +0,0 @@
-package com.thecar.admin.login.dao;
-
-import com.thecar.admin.login.vo.LoginVO;
-
-public interface LoginDao {
-	
-	public LoginVO adminLoginSelect(LoginVO lvo);
-
-}
--- thecarProject/workspace/SpringthecarProject_admin/src/main/java/com/thecar/admin/login/service/LoginService.java
+++
@@ -1,8 +0,0 @@
-package com.thecar.admin.login.service;
-import com.thecar.admin.login.vo.LoginVO;
-
-public interface LoginService {
-	
-	public LoginVO adminLoginSelect(String a_id, String a_pw);
-}
-
